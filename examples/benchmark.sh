0fddaa7d00eea84d6378ff6ef64c953b67cc291a
skwok1 <skwok1@mfe02.chicagobooth.edu>
2025-01-14 23:45:21 -0600

    must run in compute

--- examples/benchmark.sh
+++ examples/benchmark.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #
-#SBATCH --mail-user=serenkwok@cs.uchicago.edu
-#SBATCH --mail-type=ALL
+#SBATCH --account=pi-naragam
 #SBATCH --job-name=golem_benchmark
 #SBATCH --output=./slurm/out/%j.%N.stdout
 #SBATCH --error=./slurm/out/%j.%N.stderr
 #SBATCH --chdir=/home/skwok1/dagrad/examples
-#SBATCH --partition=gpu_h100
+#SBATCH --partition=standard
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
@@ -15,6 +14,10 @@
 #SBATCH --time=2-00:00:00
 
 set -e
+
+cd ..
+pip3 install -e .
+cd examples
 
 echo "Running benchmark"
 
