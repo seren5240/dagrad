26b29a5d3ebce94e69df38c56cba51cd5e674a90
skwok1 <skwok1@mfe02.chicagobooth.edu>
2025-01-16 12:51:07 -0600

    request gpu

--- examples/benchmark.sh
+++ examples/benchmark.sh
@@ -5,12 +5,12 @@
 #SBATCH --output=./slurm/out/%j.%N.stdout
 #SBATCH --error=./slurm/out/%j.%N.stderr
 #SBATCH --chdir=/home/skwok1/dagrad/examples
-#SBATCH --partition=standard
+#SBATCH --partition=gpu_h100
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=900
-#SBATCH --exclusive
+#SBATCH --gres=gpu:1
 #SBATCH --time=2-00:00:00
 
 set -e
