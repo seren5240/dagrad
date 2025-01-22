4fea50d1be5ec704fb860936485660a664b006fd
skwok1 <skwok1@mfe02.chicagobooth.edu>
2025-01-22 15:53:04 -0600

    use existing thresholder

--- examples/benchmark.py
+++ examples/benchmark.py
@@ -1,44 +1,9 @@
 from dagrad import dagrad
-from dagrad import generate_linear_data, count_accuracy, is_dag
+from dagrad import generate_linear_data, count_accuracy, threshold_till_dag
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-
-def threshold_till_dag(B):
-    """Remove the edges with smallest absolute weight until a DAG is obtained.
-
-    Args:
-        B (numpy.ndarray): [d, d] weighted matrix.
-
-    Returns:
-        numpy.ndarray: [d, d] weighted matrix of DAG.
-        float: Minimum threshold to obtain DAG.
-    """
-    if is_dag(B):
-        return B, 0
-
-    B = np.copy(B)
-    # Get the indices with non-zero weight
-    nonzero_indices = np.where(B != 0)
-    # Each element in the list is a tuple (weight, j, i)
-    weight_indices_ls = list(zip(B[nonzero_indices],
-                                 nonzero_indices[0],
-                                 nonzero_indices[1]))
-    # Sort based on absolute weight
-    sorted_weight_indices_ls = sorted(weight_indices_ls, key=lambda tup: abs(tup[0]))
-
-    for weight, j, i in sorted_weight_indices_ls:
-        if is_dag(B):
-            # A DAG is found
-            break
-
-        # Remove edge with smallest absolute weight
-        B[j, i] = 0
-        dag_thres = abs(weight)
-
-    return B, dag_thres
-
 
 def postprocess(B, graph_thres=0.3):
     """Post-process estimated solution:
