24a387c472f72cbf643c7038d87b42ffffc30db1
Seren Kwok <serenkwok5240@gmail.com>
2025-03-03 10:02:47 -0600

    grandag constrained solver

--- dagrad/flex/modules/constrained_solvers.py
+++ dagrad/flex/modules/constrained_solvers.py
@@ -1,5 +1,6 @@
 import copy
 from typing import List, Optional, Union
+import numpy as np
 import torch
 # import tqdm as tqdm
 from tqdm.auto import tqdm
@@ -239,6 +240,152 @@ class AugmentedLagrangian(ConstrainedSolver):
             if h <= self.h_tol or self.rho >= self.rho_max:
                 end = True
 
+class GrandagSolver(ConstrainedSolver):
+    def __init__(
+        self,
+        num_iter: int = 20,
+        num_steps: Optional[Union[List[int], int]] = None,
+        alpha_init: float = 0.0,
+        rho_init: float = 1.0,
+        rho_scale: float = 10.0,
+        rho_max: float = 1e8,
+        weight_decay: float = 0.0,
+        l1_coeff: float = 0.0,
+        h_tol: float = 1e-8,
+    ):
+        super().__init__()
+        self.num_iter = num_iter
+        self.num_steps = num_steps
+        self.alpha_multiplier = alpha_init
+        self.rho = rho_init
+        self.rho_scale = rho_scale
+        self.rho_max = rho_max
+        self.weight_decay = weight_decay
+        self.l1_coeff = l1_coeff
+        self.h_tol = h_tol
 
+    def validate_inputs(self):
+        if isinstance(self.num_steps, list):
+            if len(self.num_steps) < self.num_iter:
+                missing_steps = self.num_iter - len(self.num_steps)
+                self.vwarn(f"Using the first value from num_steps for the first {missing_steps} iterations")
+                self.num_steps = missing_steps * [self.num_steps[0]] + self.num_steps
+        elif isinstance(self.num_steps, (int, float)):
+            self.num_steps = self.num_iter * [self.num_steps]
+        else:
+            self.vwarn("num_steps is None, will use default value from Unconstrained Solver")
+            if self.unconstrained_solver.num_steps is None:
+                raise ValueError("num_steps is None in Unconstrained Solver")
+            else:
+                self.num_steps = self.num_iter * [self.unconstrained_solver.num_steps]
+        
+        if self.l1_coeff > 0:
+            # make sure the function l1_loss is implemented in the model
+            if not hasattr(self.model, "l1_loss"):
+                raise ValueError("Model does not have l1_loss method")
+
+    def solve(self, dataset, model, unconstrained_solver, loss_fn, dag_fn, omega_lambda=1e-4, stop_crit_win=100):
+        torch.set_default_dtype(self.dtype)
+        self.model = model
+        self.unconstrained_solver = unconstrained_solver
+        self.vprint("Using Augmented Lagrangian Method to Solve the DAG constrained Problem")
+        self.validate_inputs()
+
+        end = False
+
+        not_nlls = []  # Augmented Lagrangrian minus (pseudo) NLL
+        aug_lagrangians_val = []
+        nlls_val = []  # NLL on validation
+        hs = []
+        for i in range(self.num_iter):
+            if end:
+                continue
+            def augmented_loss(target):
+                model.train()
+                # Original loss
+                # loss = loss_fn(output, target)
+                weights, biases = model.get_parameters()
+                loss = - torch.mean(model.compute_log_likelihood(target, weights, biases))
+                # print(f'total loss: {total_loss}')
+                model.eval()
+                
+                # L2 regularization
+                if self.weight_decay > 0:
+                    l2_loss = 0.0
+                    for param in model.parameters():
+                        l2_loss += torch.sum(param**2)
+                    loss += 0.5 * self.weight_decay * l2_loss
+                    
+                if self.l1_coeff > 0:
+                    loss += self.l1_coeff * model.l1_loss()
+                    
+                # DAG constraint
+                h = dag_fn(model.adj())
+                hs.append(h.item())
+                
+                # Augmented Lagrangian terms
+                alm_term = self.alpha_multiplier * h + 0.5 * self.rho * h**2
+                not_nlls.append(alm_term.item())
+                return loss + alm_term
+
+            success = False
+            model_copy = model.state_dict().copy()
+            lr_scale = None
+            lr_scheduler = False
+
+            while not success:
+                self.vprint(
+                    f"\nSolving ALM iteration #{i+1} with lambda={self.alpha_multiplier:.3f}, "
+                    f"rho={self.rho:.3f} for {self.num_steps[i]} steps"
+                )
+                
+                unconstrained_solver.num_steps = self.num_steps[i]
+                success = unconstrained_solver(
+                    dataset, model, augmented_loss, dag_fn, lr_scale, lr_scheduler
+                )
+
+                if not success:
+                    model.load_state_dict(model_copy)
+                    lr_scale = 0.5 if lr_scale is None else 0.5 * lr_scale
+                    lr_scheduler = True
+                    if lr_scale < 1e-6:
+                        break
+            
+            if i % stop_crit_win == 0:
+                with torch.no_grad():
+                    loss_val = augmented_loss(dataset)
+                    nlls_val.append(loss_val)
+                    aug_lagrangians_val.append([i, loss_val + not_nlls[-1]])
+                    
+            if i >= 2 * stop_crit_win and i % (2 * stop_crit_win) == 0:
+                t0, t_half, t1 = aug_lagrangians_val[-3][1], aug_lagrangians_val[-2][1], aug_lagrangians_val[-1][1]
+
+                # if the validation loss went up and down, do not update lagrangian and penalty coefficients.
+                if not (min(t0, t1) < t_half < max(t0, t1)):
+                    delta_lambda = -np.inf
+                else:
+                    delta_lambda = (t1 - t0) / stop_crit_win
+            else:
+                delta_lambda = -np.inf  # do not update lambda nor mu
+            
+            if hs[-1] > self.h_tol:
+                if abs(delta_lambda) < omega_lambda or delta_lambda > 0:
+                    self.alpha_multiplier += self.rho * hs[-1]
+
+                    # Did the constraint improve sufficiently?
+                    if len(hs) >= 2:
+                        if hs[-1] > 0.9 * hs[-2]:
+                            self.rho *= self.rho_scale
+
+                    with torch.no_grad():
+                        gap_in_not_nll = 0.5 * self.rho * hs[-1] ** 2 + self.alpha_multiplier * hs[-1] - not_nlls[-1]
+                        aug_lagrangians_val[-1][1] += gap_in_not_nll
+
+            else:
+                with torch.no_grad():
+                    w_adj = model.adj()
+                    to_keep = (w_adj > 0).type(torch.Tensor)
+                    model.adjacency *= to_keep
+                end = True
 
 # TODO: Implement TOPOSolver (Bilevel)
