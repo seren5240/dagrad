164dac290390cecf2667ff92a13c7093511d7148
skwok1 <skwok1@mfe01.chicagobooth.edu>
2025-03-09 10:45:47 -0500

    flatten parallelization

--- benchmark/benchmarker.py
+++ benchmark/benchmarker.py
@@ -3,11 +3,7 @@ import joblib
 from numpy import ndarray
 import numpy as np
 from joblib import Parallel, delayed
-from multiprocessing import Lock
 from dagrad.utils import utils
-
-file_lock = Lock()
-
 
 def run_one_trial(
     n: int,
@@ -46,49 +42,6 @@ def run_one_trial(
         results[name] = acc["shd"] / d
     return results
 
-
-def run_one_benchmark(
-    n: int,
-    d: int,
-    edges: int,
-    sem_type: str,
-    noise_type: str,
-    error_var: str,
-    linearity: str,
-    graph_type: str,
-    benchmark_fns: dict[str, Callable[[ndarray], ndarray]],
-    trials: int,
-    output_filename: str,
-):
-    results = Parallel(n_jobs=-1, backend="loky")(
-        delayed(run_one_trial)(
-            n,
-            d,
-            edges,
-            sem_type,
-            noise_type,
-            error_var,
-            linearity,
-            graph_type,
-            benchmark_fns,
-        )
-        for _ in range(trials)
-    )
-    aggregated_results = {
-        name: np.mean([trial[name] for trial in results])
-        for name in benchmark_fns.keys()
-    }
-
-    output_lines = [
-        f"{method},{n},{d},{edges},{noise_type},{error_var},{linearity},{graph_type},{aggregated_results[method]}\n"
-        for method in benchmark_fns.keys()
-    ]
-
-    with file_lock:
-        with open(output_filename, "a") as f:
-            f.writelines(output_lines)
-
-
 def run_benchmarks(
     n: int,
     sizes: list[tuple[int, int]],
@@ -102,51 +55,42 @@ def run_benchmarks(
     sem_type: str = "mlp",
 ):
     """
-    Run benchmarks on multiple vertex/edge combinations and benchmark functions.
-
-    Parameters
-    ----------
-    n: int
-        Number of samples
-    sizes: list[tuple[int, int]]
-        List of node/edge combinations
-    noise_type: list[str]
-        list of ``gauss``, ``exp``, ``gumbel``, ``uniform``, ``logistic``, ``poisson``
-    error_var: str
-        list of ``eq``, ``random``
-    linearities: str
-        list of ``linear``, ``nonlinear``
-    graph_types: str
-        list of ``ER``, ``SF``, ``BP``
-    sem_type: str
-        ``mlp``, ``mim``, ``gp``, ``gp-add``. Only applicable for nonlinear models.
+    Run benchmarks on multiple vertex/edge combinations and benchmark functions,
+    flattening the nested parallelism so that each trial is scheduled as an individual task.
     """
-
     num_cores = joblib.cpu_count()
     print(f"Detected {num_cores} CPU cores. Running benchmarks in parallel.")
 
-    with open(output_filename, "w") as f:
-        f.write(
-            "method,n,d,edges,noise_type,error_var,linearity,graph_type,mean_normalized_shd\n"
-        )
+    tasks = []
+    keys = []
+    for d, edges in sizes:
+        for noise_type in noise_types:
+            for error_var in error_vars:
+                for linearity in linearities:
+                    for graph_type in graph_types:
+                        for _ in range(trials):
+                            tasks.append(
+                                delayed(run_one_trial)(
+                                    n, d, edges, sem_type, noise_type, error_var, linearity, graph_type, benchmark_fns
+                                )
+                            )
+                            keys.append((n, d, edges, noise_type, error_var, linearity, graph_type))
 
-    Parallel(n_jobs=-1, backend="loky")(
-        delayed(run_one_benchmark)(
-            n,
-            d,
-            edges,
-            sem_type,
-            noise_type,
-            error_var,
-            linearity,
-            graph_type,
-            benchmark_fns,
-            trials,
-            output_filename,
-        )
-        for d, edges in sizes
-        for noise_type in noise_types
-        for error_var in error_vars
-        for linearity in linearities
-        for graph_type in graph_types
-    )
+    results = Parallel(n_jobs=-1, backend="loky")(tasks)
+
+    aggregated = {}
+    for key, res in zip(keys, results):
+        if key not in aggregated:
+            aggregated[key] = {method: [] for method in benchmark_fns.keys()}
+        for method, value in res.items():
+            aggregated[key][method].append(value)
+
+    with open(output_filename, "w") as f:
+        f.write("method,n,d,edges,noise_type,error_var,linearity,graph_type,mean_normalized_shd\n")
+        for key, method_vals in aggregated.items():
+            n, d, edges, noise_type, error_var, linearity, graph_type = key
+            for method, values in method_vals.items():
+                mean_normalized_shd = np.mean(values)
+                f.write(
+                    f"{method},{n},{d},{edges},{noise_type},{error_var},{linearity},{graph_type},{mean_normalized_shd}\n"
+                )
