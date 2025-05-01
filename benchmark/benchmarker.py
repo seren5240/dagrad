934f8d24ab347e49b203b0d7df02d4ec94e43fd5
skwok1 <skwok1@mfe02.chicagobooth.edu>
2025-05-01 12:20:18 -0500

    restore limits

--- benchmark/benchmarker.py
+++ benchmark/benchmarker.py
@@ -5,6 +5,14 @@ import numpy as np
 from joblib import Parallel, delayed
 from dagrad.utils import utils
 import os
+import torch
+
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+torch.set_num_threads(1)
 
 
 def create_one_dataset(
